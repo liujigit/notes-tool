0705fea471a4dcf13eb556bd5a91a0af4e75cb60
liu ji <13051938839@163.com>
2023-12-19 23:31:06 +0800

    init

---
+++ src/main/java/one/platform/plugin/config/NoteIcons.java
@@ -0,0 +1,14 @@
+package one.platform.plugin.config;
+
+import com.intellij.icons.AllIcons;
+
+import javax.swing.*;
+
+/**
+ * @author liuji
+ * @date 2023/12/25 23:24
+ **/
+public class NoteIcons {
+
+    public static final Icon  NOTE_ICON = AllIcons.Ide.LikeSelected;
+}
